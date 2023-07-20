08517abb5bb4b06652322b6edd18baaa8debfb73
Michael Friedman <minorpianokeys@gmail.com>
2023-07-20 17:07:17 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy Birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+
+function writeCards(names, event) {
+    let newNames = [];
+
+    for (let i = 0; i < names.length; i++) {
+        newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return newNames;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
